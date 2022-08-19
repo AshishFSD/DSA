6b3cf2aecbe30f8e9ea7f96117ab36b79229508b
ASHISH KUMAt <akumaraman966@gmail.com>
2022-08-19 15:34:11 +0530

    update

---
+++ src/PrintArry.java
@@ -0,0 +1,13 @@
+public class PrintArry {
+
+     public static  void main(String[] args){
+
+         int[] arr ={10,23,4,67,8,90,23,55};
+
+
+          for (int i=0; i<arr.length; i++){
+
+              System.out.println(arr[i]);
+          }
+     }
+}
